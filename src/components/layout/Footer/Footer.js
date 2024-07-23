a137eeaacee7a9e1d298f51e158c0eab1569985d
Renan <renanandreolla@outlook.com>
2024-07-23 18:44:35 -0300

    feat: adjust footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -4,11 +4,12 @@ import React, { useState } from "react";
 function Footer() {
 
   return (
-    <div className="text-center text-muted">
-        <span className="dev-info small">DEMA PRODUTOS AUTOMOTIVOS</span>
-        <span className="dev-info small">R. Alm. Barroso - Centro, Erechim - RS, 99700-122</span>
-        <span className="dev-info small">Desenvolvido por: © <a className="renan" href="https://www.linkedin.com/in/renan-andreolla-a60685190" target="_blank">Renan Andreolla</a></span>
-    </div>
+    <></>
+    // <div className="text-center text-muted">
+    //     <span className="dev-info small">DEMA PRODUTOS AUTOMOTIVOS</span>
+    //     <span className="dev-info small">R. Alm. Barroso - Centro, Erechim - RS, 99700-122</span>
+    //     <span className="dev-info small">Desenvolvido por: © <a className="renan" href="https://www.linkedin.com/in/renan-andreolla-a60685190" target="_blank">Renan Andreolla</a></span>
+    // </div>
   );
 }
 
