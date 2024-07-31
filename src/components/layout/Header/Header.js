50b285fc681024925a55dfcea46195c78ee62d03
Renan <renanandreolla@outlook.com>
2024-07-30 23:30:46 -0300

    feat: icon wpp

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -4,18 +4,29 @@ import CartIcon from '../Cart/CartIcon';
 
 function Header() {
 
+  const sendWpp = () => {
+    var message = "Olá, vim pelo seu site! Gostaria de tirar algumas dúvidas!";
+    const phoneNumber = '5554999101433';
+    const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+  }
+
   return (
-    <div className="header">
-      <div className="header-left">
-        <a href="/">
-          <img className="img-logo" src={`${process.env.PUBLIC_URL}/Dema-logo-2.png`} alt="Logo" />
-        </a>
+    <>
+      <div className="header">
+        <div className="header-left">
+          <a href="/">
+            <img className="img-logo" src={`${process.env.PUBLIC_URL}/Dema-logo-2.png`} alt="Logo" />
+          </a>
+        </div>
+        
+        <div className="header-right">
+          <CartIcon />
+        </div>
       </div>
-      
-      <div className="header-right">
-        <CartIcon />
-      </div>
-    </div>
+
+      <img className="img-wpp" src={'https://imagepng.org/whatsapp-icone-icon/whatsapp-icone-1/'} onClick={sendWpp} alt="Logo" />
+    </>
   );
 }
 
