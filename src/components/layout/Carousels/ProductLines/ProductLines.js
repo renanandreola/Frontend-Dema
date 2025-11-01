9715b2a68c041aa55ce001bbe23717eb0db41008
Renan <renanandreolla@outlook.com>
2025-11-01 15:45:10 -0300

    up

--- src/components/layout/Carousels/ProductLines/ProductLines.js
+++ src/components/layout/Carousels/ProductLines/ProductLines.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./ProductLines.css";
 
 const products = [
-  { title: "QUÍMICOS", image: "https://i.imgur.com/NpLWUQm.jpeg", link: "http://localhost:3001/category/6904e0783865d9fe1e7c7000" },
-  { title: "ACESSÓRIOS", image: "https://i.imgur.com/9G8Nz5S.jpeg", link: "http://localhost:3001/category/6904e09c3865d9fe1e7c7001" },
-  { title: "LIMPEZA PESADA", image: "https://i.imgur.com/TS7FOAf.jpeg", link: "http://localhost:3001/category/6904e0a53865d9fe1e7c7002" },
-  { title: "POLIMENTO", image: "https://i.imgur.com/Imo8NOM.jpeg", link: "http://localhost:3001/category/6904e0c23865d9fe1e7c7003" },
-  { title: "AEROSSOL", image: "https://i.imgur.com/cHOpzRg.jpeg", link: "http://localhost:3001/category/6904e0dc3865d9fe1e7c7004" },
-  { title: "CERAMIC COATING", image: "https://i.imgur.com/dk8S1ho.png", link: "http://localhost:3001/category/6904e0f53865d9fe1e7c7005" },
-  { title: "LIMPEZA INTERNA", image: "https://i.imgur.com/fj6d9LQ.jpeg",link: "http://localhost:3001/category/6904e0fe3865d9fe1e7c7006" },
-  { title: "EQUIPAMENTOS", image: "https://i.imgur.com/LQC47pm.jpeg", link: "http://localhost:3001/category/6904e1063865d9fe1e7c7007" },
+  { title: "QUÍMICOS", image: "https://i.imgur.com/NpLWUQm.jpeg", link: "https://demautomotive.vercel.app/category/6904e0783865d9fe1e7c7000" },
+  { title: "ACESSÓRIOS", image: "https://i.imgur.com/9G8Nz5S.jpeg", link: "https://demautomotive.vercel.app/category/6904e09c3865d9fe1e7c7001" },
+  { title: "LIMPEZA PESADA", image: "https://i.imgur.com/TS7FOAf.jpeg", link: "https://demautomotive.vercel.app/category/6904e0a53865d9fe1e7c7002" },
+  { title: "POLIMENTO", image: "https://i.imgur.com/Imo8NOM.jpeg", link: "https://demautomotive.vercel.app/category/6904e0c23865d9fe1e7c7003" },
+  { title: "AEROSSOL", image: "https://i.imgur.com/cHOpzRg.jpeg", link: "https://demautomotive.vercel.app/category/6904e0dc3865d9fe1e7c7004" },
+  { title: "CERAMIC COATING", image: "https://i.imgur.com/dk8S1ho.png", link: "https://demautomotive.vercel.app/category/6904e0f53865d9fe1e7c7005" },
+  { title: "LIMPEZA INTERNA", image: "https://i.imgur.com/fj6d9LQ.jpeg",link: "https://demautomotive.vercel.app/category/6904e0fe3865d9fe1e7c7006" },
+  { title: "EQUIPAMENTOS", image: "https://i.imgur.com/LQC47pm.jpeg", link: "https://demautomotive.vercel.app/category/6904e1063865d9fe1e7c7007" },
 ];
 
 export default function ProductLines() {
